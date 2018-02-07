968ac4842f3a5c94bf426090f6008466bca3a091
samuelloh <e0031610@u.nus.edu>
2018-02-07 10:31:57 +0800

    Added block, street, unit and postal code classes

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -8,11 +8,21 @@ import seedu.addressbook.data.exception.IllegalValueException;
  */
 public class Address {
 
-    public static final String EXAMPLE = "123, some street";
-    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
-    public static final String ADDRESS_VALIDATION_REGEX = ".+";
+    public static final String EXAMPLE = "123, some street, #01-234, 123456";
+    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses must be in the format of BLOCK, STREET, UNIT, POSTAL CODE";
+    public static final String ADDRESS_VALIDATION_REGEX = ".+,.+,.+,.+";
 
-    public final String value;
+
+    private final int BLOCK = 0;
+    private final int STREET = 1;
+    private final int UNIT = 2;
+    private final int POSTALCODE = 3;
+
+
+    public Block block;
+    public Street street;
+    public Unit unit;
+    public PostalCode postalCode;
     private boolean isPrivate;
 
     /**
@@ -23,10 +33,24 @@ public class Address {
     public Address(String address, boolean isPrivate) throws IllegalValueException {
         String trimmedAddress = address.trim();
         this.isPrivate = isPrivate;
+
         if (!isValidAddress(trimmedAddress)) {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
-        this.value = trimmedAddress;
+        processAddress(trimmedAddress);
+    }
+
+    /**
+     * processes different components of address(block,street,unit,postal code)
+     * @param trimmedAddress
+     */
+    public void processAddress(String trimmedAddress){
+        String[] components = trimmedAddress.split(",");
+        block = new Block(components[BLOCK].trim());
+        street = new Street(components[STREET].trim());
+        unit = new Unit(components[UNIT].trim());
+        postalCode = new PostalCode(components[POSTALCODE].trim());
+
     }
 
     /**
@@ -36,21 +60,28 @@ public class Address {
         return test.matches(ADDRESS_VALIDATION_REGEX);
     }
 
+    /**
+     * returns components of address in the form of BLOCK, STREET, UNIT, POSTAL CODE
+     */
+    public String getAddress(){
+        return block.toString() + ", "  + street.toString() + ", " + unit.toString() + ", " + postalCode.toString();
+    }
+
     @Override
     public String toString() {
-        return value;
+        return getAddress();
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof Address // instanceof handles nulls
-                && this.value.equals(((Address) other).value)); // state check
+                && getAddress().equals(((Address) other).getAddress())); // state check
     }
 
     @Override
     public int hashCode() {
-        return value.hashCode();
+        return getAddress().hashCode();
     }
 
     public boolean isPrivate() {
---
+++ src/seedu/addressbook/data/person/Block.java
@@ -0,0 +1,15 @@
+package seedu.addressbook.data.person;
+
+public class Block {
+    public final String block;
+
+    public Block(String block){
+        this.block = block;
+    }
+
+    public String toString(){
+        return block;
+    }
+
+}
+
